8f46766888bcd2d75c33494ff6c8bebda51b01b4
Abdulrahman <vegeta444555@gmail.com>
2026-09-03 18:43:03 +0300

    Implement Coursology test page detection

--- src/content/content.js
+++ src/content/content.js
@@ -1,1 +1,10 @@
+function isCoursologyUsmleStep1TestPage(url) {
+	const pageUrl = new URL(url);
+
+	return pageUrl.protocol === "https:" &&
+		pageUrl.hostname === "coursology-qbank.com" &&
+		/^\/qbanks\/usmle1\/tests\/\d+$/.test(pageUrl.pathname);
+}
+
+const isSupportedTestPage = isCoursologyUsmleStep1TestPage(window.location.href);
 
